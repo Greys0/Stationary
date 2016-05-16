849e1aa78e1e15a622f4ea32559976b349d16761
Greys0 <lily227@gmail.com>
2016-05-15 22:24:32 -0400

    fixed yer bullshit (reverted from commit f00d0693f8bf28464b7aeb63f20d625fec60f785)

--- StationaryVesselModule.cs
+++ StationaryVesselModule.cs
@@ -13,57 +13,57 @@ namespace StationaryVessels
 		private Vessel vessel;
 		private List<StationaryModule> modules;
 
-		public void Awake() //compiles fine
+		new public void Awake ()
 		{
-			Debug.Log("StationaryAwake");
-			vessel = GetComponent<Vessel>();
+			Debug.Log ("StationaryAwake");
+			vessel = GetComponent<Vessel> ();
 
-			//Add StationaryModule to all controllable parts on vessel
-			if(!vessel.isEVA)
+			// Add StationaryModule to all controllable parts on vessel
+			if (!vessel.isEVA)
 			{
-				foreach(Part p in vessel.parts)
+				foreach (Part p in vessel.parts)
 				{
-					if(p.Modules.Contains("ModuleCommand")
-					&& !p.Modules.Contains("StationaryModule"))
+					if (p.Modules.Contains ("ModuleCommand")
+					&& !p.Modules.Contains ("StationaryModule"))
 					{
-						p.AddModule("StationaryModule");
+						p.AddModule ("StationaryModule");
 					}
 				}
 			}
 		}
 
-		public void Start()
+		public void Start ()
 		{
-			Debug.Log("StationaryStart");
+			Debug.Log ("StationaryStart");
 
 			RebuildModulesList();
 
-			//Establish loaded state
-			if(modules.FindAll(x => x.isFrozen).Count > 0) //another redundant == true
+			// Establish loaded state
+			if (modules.FindAll(x => x.isFrozen == true).Count > 0)
 			{
 				setFreeze(true);
 			}
 		}
 
-		//TODO: Will probably need to call this to fix the list after some events like docking and breaks
+		// Todo: Will probably need to call this to fix the list after some events like docking and breaks
 		private void RebuildModulesList()
 		{
-			if(modules == null) { modules = new List<StationaryModule>(); }
-			List<Part> parts = vessel.parts.FindAll(p => p.Modules.Contains("StationaryModule")); //comparison with true is redundant
+			if (modules == null) { modules = new List<StationaryModule>(); }
+			List<Part> parts = vessel.parts.FindAll(p => p.Modules.Contains("StationaryModule") == true);
 
-			foreach(Part p in parts)
+			foreach (Part p in parts)
 			{
 				modules.AddRange(p.Modules.GetModules<StationaryModule>());
 			}
 		}
 
-		public void setFreeze(bool state)
+		public void setFreeze (bool state)
 		{
-			foreach(Part p in vessel.parts)
+			foreach (Part p in vessel.parts)
 			{
-				//Skip parts without Rigidbodies
-				if(p.GetComponent<Rigidbody>() == null) continue;
-				p.GetComponent<Rigidbody>().isKinematic = state;
+				// Skip parts without Rigidbodys
+				if (p.GetComponent<Rigidbody> () == null) continue;
+				p.GetComponent<Rigidbody> ().isKinematic = state;
 			}
 			Propogate(state);
 		}
